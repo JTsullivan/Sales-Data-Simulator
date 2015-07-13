fbdb113a051f7accd1151a148f6ac0b9744cda88
JTsullivan <jt.sullivan.iii@gmail.com>
2015-07-12 23:58:31 -0600

    asdf

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace AutoSalesDataSheetGeneratorCsharp
+{
+    partial class AutoSalesGeneratorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PrimaryActionButton = new System.Windows.Forms.Button();
+            this.UIfilenameBOX = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // PrimaryActionButton
+            // 
+            this.PrimaryActionButton.Font = new System.Drawing.Font("Microsoft JhengHei UI", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PrimaryActionButton.Location = new System.Drawing.Point(39, 50);
+            this.PrimaryActionButton.Name = "PrimaryActionButton";
+            this.PrimaryActionButton.Size = new System.Drawing.Size(203, 147);
+            this.PrimaryActionButton.TabIndex = 0;
+            this.PrimaryActionButton.Text = "K";
+            this.PrimaryActionButton.UseVisualStyleBackColor = true;
+            this.PrimaryActionButton.Click += new System.EventHandler(this.PrimaryActionButton_Click);
+            // 
+            // UIfilenameBOX
+            // 
+            this.UIfilenameBOX.Location = new System.Drawing.Point(39, 229);
+            this.UIfilenameBOX.Name = "UIfilenameBOX";
+            this.UIfilenameBOX.Size = new System.Drawing.Size(203, 20);
+            this.UIfilenameBOX.TabIndex = 1;
+            this.UIfilenameBOX.TextChanged += new System.EventHandler(this.UIfilenameBOX_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 213);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "File Name";
+            // 
+            // AutoSalesGeneratorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.UIfilenameBOX);
+            this.Controls.Add(this.PrimaryActionButton);
+            this.Name = "AutoSalesGeneratorForm";
+            this.Text = "Auto Sales Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button PrimaryActionButton;
+        private System.Windows.Forms.TextBox UIfilenameBOX;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AutoSalesDataSheetGeneratorCsharp
+{
+    public partial class AutoSalesGeneratorForm : Form
+    {
+        public AutoSalesGeneratorForm()
+        {
+            InitializeComponent();
+        }
+
+        public static string filePath;
+
+        private void PrimaryActionButton_Click(object sender, EventArgs e)
+        {
+            Random entropy = new Random();
+            
+            if(filePath != null)
+            {
+                string[] customerArray = {"Qualcomm","Samsung","Elpida","ZTE","Hauwei","Nokia","Apple"};
+                double[] hiPrices = {8.88,12,10,9.25,9.5,9.75,5};
+                double[] loPrices = {5.85,8.5,6.25,6.5,8.5,4.9,4.95};
+                int[] maxQTY = {25000,8500,4500,8000,7500,15000,20000};
+                int[] minQTY = {20000,1000,2500,8000,5000,10000,1000};
+
+            
+
+                int recordsToCreateCount = 1000;
+
+                using(StreamWriter theWriter = new StreamWriter(filePath))
+                {
+                    for(int i = 0; i < recordsToCreateCount; i++)
+                    {
+
+                        int manufacturerINDEX = entropy.Next(0,customerArray.Length);
+                        string customer = customerArray[manufacturerINDEX];
+                        string qty = entropy.Next(minQTY[manufacturerINDEX],maxQTY[manufacturerINDEX]).ToString();
+                        string price = entropy.Next((int)loPrices[manufacturerINDEX],(int)hiPrices[manufacturerINDEX]).ToString();
+                        string chipType;
+                        double coinFlipForChip = entropy.NextDouble();
+                        if(coinFlipForChip >= 0.7)
+                        {                       
+                            chipType = "BitShareA";                           
+                        }
+                        else
+                        {
+                            price = (Convert.ToInt32(price) + 1).ToString();
+                            chipType = "BitShareB";
+                        }
+
+                        theWriter.WriteLine(customer + "," + chipType + "," + qty + "," + price);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a File Name");
+            }
+        }
+
+        private void UIfilenameBOX_TextChanged(object sender, EventArgs e)
+        {
+            filePath = UIfilenameBOX.Text + ".csv";
+        }
+    }
+}
